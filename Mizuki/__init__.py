548c2152640958fe924521bd4258c217f3947c98
Shashank sharma <78837537+theshashankk@users.noreply.github.com>
2021-04-16 11:01:53 +0530

    Update __init__.py

--- Mizuki/__init__.py
+++ Mizuki/__init__.py
@@ -98,7 +98,7 @@ if ENV:
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/ImJanindu/Mizuki"
+        "UPSTREAM_REPO", "https://github.com/theshashankk/Hentao-ROBO"
     )
     BOT_ID = int(os.environ.get("BOT_ID", None))
     OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", None)
